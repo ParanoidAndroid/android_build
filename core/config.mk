83c983db988482e9d9555a9ef59a8bc296ba1ec3
aaronpoweruser <atlrudeboi1@gmail.com>
2012-12-10 10:52:02 -0500

    Use GCC 4.7 from google
    Noticible preformance improvement
    Fixes needed
    *Framework/rs
    *System/security
    *external/oprofile
    *external/elfutils
    *external/v8
    
    Change-Id: Ic1eadf1fa7df1525471b9c844ac0c4bd9e778330

--- core/config.mk
+++ core/config.mk
@@ -226,9 +226,7 @@ endif
 TARGET_CPU_ABI2 := $(strip $(TARGET_CPU_ABI2))
 
 # default target GCC version
-ifeq ($(strip $(TARGET_GCC_VERSION)),)
-TARGET_GCC_VERSION := 4.6
-endif
+TARGET_GCC_VERSION := 4.7
 
 # $(1): os/arch
 define select-android-config-h
