30eb25b947dcab91667597934dfc4ea4a1fd6d67
wjb <jaybob413@gmail.com>
2013-02-03 09:43:05 -0500

    Remove duplicate vendorsetup.sh processing
    Change-Id: I003ebf4768224431c481a7734acccb1bf6cb49f5

--- envsetup.sh
+++ envsetup.sh
@@ -1187,12 +1187,4 @@ do
 done
 unset f
 
-# Execute the contents of any vendorsetup.sh files we can find.
-for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
-do
-    echo "including $f"
-    . $f
-done
-unset f
-
 addcompletions
