fe50d94c32dc52c62f56f82d53c2aeb7d853162c
Steve Kondik <shade@chemlab.org>
2013-02-16 19:03:02 -0800

    build: Relax Cortex CFLAGS
     * Always set march=armv7-a
     * Use mtune instead of mcpu. Bad code is generated on A15 otherwise.
    
    Change-Id: Idf8ced8eb0e9d3d4676ca8ec69bf56aed5533bf4
    
    Conflicts:
    	core/combo/arch/arm/armv7-a.mk

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -11,23 +11,15 @@ ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_TLS_REGISTER      := true
 ARCH_ARM_HAVE_VFP               := true
 
-mcpu-arg = $(shell sed 's/^-mcpu=//' <<< "$(call cc-option,-mcpu=$(1),-mcpu=$(2))")
 
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a15)
-TARGET_ARCH_VARIANT_CPU := $(call mcpu-arg,cortex-a15,cortex-a9)
-endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a9)
-TARGET_ARCH_VARIANT_CPU := $(call mcpu-arg,cortex-a9,cortex-a8)
-endif
-ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a8)
-TARGET_ARCH_VARIANT_CPU := $(call mcpu-arg,cortex-a8,)
-endif
+arch_variant_cflags := \
+    -march=armv7-a
 
 ifneq ($(strip $(TARGET_ARCH_VARIANT_CPU)),)
-arch_variant_cflags := \
-    -mcpu=$(strip $(TARGET_ARCH_VARIANT_CPU))
-else
-# fall back on generic tunning if cpu is not specified
+arch_variant_cflags += \
+    -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
+endif
+
 
 arch_variant_cflags := \
     -march=armv7-a \
