d7001f503579ce8b1f3b622bf5008a48e1809cde
aaronpoweruser <atlrudeboi1@gmail.com>
2013-02-24 03:45:18 -0800

    Build fix
    Change-Id: Idfdcbce3badea6c778249be6719cf88374effa6d

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -9,8 +9,15 @@ ARCH_ARM_HAVE_CLZ               := true
 ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_TLS_REGISTER      := true
+ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
 ARCH_ARM_HAVE_VFP               := true
-
+else
+ARCH_ARM_HAVE_VFP               := false
+endif
+ifeq ($(TARGET_ARCH_VARIANT_FPU), neon)
+ARCH_ARM_HAVE_VFP_D32           := true
+ARCH_ARM_HAVE_NEON              := true
+endif
 
 arch_variant_cflags := \
     -march=armv7-a
@@ -20,11 +27,19 @@ arch_variant_cflags += \
     -mtune=$(strip $(TARGET_ARCH_VARIANT_CPU))
 endif
 
+ifneq ($(strip $(TARGET_ARCH_VARIANT_FPU)),)
+arch_variant_cflags += \
+	-mfloat-abi=softfp \
+	-mfpu=$(strip $(TARGET_ARCH_VARIANT_FPU))
+else
+# fall back on soft tunning if fpu is not specified
+arch_variant_cflags += \
+	-mfloat-abi=soft
+endif
 
-arch_variant_cflags := \
-    -march=armv7-a \
-    -mfloat-abi=softfp \
-    -mfpu=vfpv3-d16
-
+ifeq ($(strip $(TARGET_ARCH_VARIANT_CPU)),cortex-a8)
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
+else
+arch_variant_ldflags :=
+endif
